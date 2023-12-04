0e75e89bd1060a5acbe7551afc5c0eb0977a9c9c
EmilienBois <emilienbois974@gmail.com>
2023-12-04 09:48:30 +0100

    added utils

---
+++ utils/utils.go
@@ -0,0 +1,25 @@
+package utils
+
+import (
+	"bufio"
+	"os"
+)
+
+// Lecture de fichier
+func read_file(path string) ([]string, int) {
+	f, _ := os.Open(path)
+
+	scanner := bufio.NewScanner(f)
+
+	var newMeasureString string
+	var file []string
+	i := 0
+	for scanner.Scan() {
+
+		newMeasureString = scanner.Text()
+		// adding to the list
+		file = append(file, newMeasureString)
+		i += 1
+	}
+	return file, i
+}
